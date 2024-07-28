1692131f98f550e18f07d90fd2cd05c749b56cdd
Project 2025 <proj2025@pm.me>
2024-07-27 17:31:56 -0700

    Initial

---
+++ content.js
@@ -0,0 +1,36 @@
+module.exports = {
+  sections: [
+    {
+      title: 'Forward: A Promise to America',
+      author: 'Kevin D. Roberts, PhD',
+      quotes: [
+        {
+          page: 4, ref: 'page/n35', text: `
+                deleting the terms sexual orientation and gender identity … gender, gender equality, gender equity,
+                gender awareness, gender-sensitive, abortion, reproductive health, reproductive rights … out of
+                every … rule, … regulation, contract, grant, regulation, and … legislation
+                `
+        },
+        {
+          page: 6, ref: 'page/n37', text: `
+                the Dobbs decision is just the beginning. … push as hard as possible to protect the unborn in every
+                jurisdiction in America. In particular, the next conservative President should work with Congress to
+                enact the most robust protections for the unborn
+                `
+        }
+      ]
+    },
+    {
+      title: 'Section One: Taking the Reins of Government',
+      quotes: [
+        {
+          page: 20, ref: 'page/n51', text: `
+                political appointees must be given the tools, knowledge, and support to overcome the federal government’s
+                obstructionist Human Resources departments.More fundamentally, the new Administration must fill its
+                ranks with political appointees.
+                `
+        }
+      ]
+    }
+  ]
+}
