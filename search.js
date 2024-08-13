f669835d91f44e2a1a53f3042baa76b597ed6abf
Project 2025 <proj2025@pm.me>
2024-08-12 21:25:15 -0700

    query param version

--- search.js
+++ search.js
@@ -1,14 +1,12 @@
-/* global $search */
+/* global $filter */
 
-$search.onkeyup = () => {
-  const search = $search.value.trim().toLowerCase()
-  if (search.length === 0) {
-    return
-  }
+const filter = new URLSearchParams(document.location.search).get('filter')
+
+if (filter) {
   document.querySelectorAll('p').forEach($p => {
     const $note = $p.nextElementSibling
     const hasNote = $note && $note.tagName === 'ASIDE'
-    if ($p.innerText.toLowerCase().includes(search)) {
+    if ($p.innerText.toLowerCase().includes(filter.trim().toLowerCase())) {
       $p.style.display = 'block'
       if (hasNote) {
         $note.style.display = 'block'
@@ -24,9 +22,11 @@ $search.onkeyup = () => {
     let display = 'none'
     $section.querySelectorAll('p').forEach($p => {
       if ($p.style.display === 'block') {
-        display = 'block'
+        display = 'grid'
       }
     })
     $section.style.display = display
   })
+
+  $filter.value = filter
 }
